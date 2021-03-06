a952ce1145f1b6b6fa8923633897692959ebfcd8
simone rodigari <simone.rodigari@gmail.com>
2021-03-06 10:27:33 +0000

    Initial commit:
     * infra script to authenticate, create/destroy disk/image/VM, start/stop VM
     * startup script to install snapd and multipass using snap
     * README.md with description/instructions

---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# https://snapcraft.io/docs/installing-snap-on-debian
+
+sudo apt install snapd -y
+sudo snap install core
+
+# https://multipass.run/docs/installing-on-linux
+
+sudo snap install multipass
